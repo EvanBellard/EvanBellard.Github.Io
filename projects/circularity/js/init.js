4576c73dfbea057fa574ae1586fe3e8850e4e7bf
EvanBellard <eb0519919@lpssonline.com>
2025-02-27 17:32:38 +0000

    2/27/2025

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,15 +20,27 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+var circle;
+var circles = []
 
         // TODO 2 : Create a function that draws a circle 
-        
-
+        function drawCircle(){
+    // Code to draw a circle
+circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas, 10, 10);
+view.addChild(circle);
+circles.push(circle);
+        }
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
-        ////////////////////////////////////////////////////////////
+for(var i = 0; i < 2500 ; i++){
+    drawCircle()
+}
+//drawCircle()
+//drawCircle()
+//drawCircle()
+//drawCircle()
+//drawCircle()
+////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
         
@@ -37,18 +49,19 @@ var init = function (window) {
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-        function update() {
+
+        function update(){
             // TODO 4 : Update the circle's position //
 
-            
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
            
 
             // TODO 9 : Iterate over the array
-           
-            
+           for(var i = 0; i < circles.length; i++){
+            physikz.updatePosition(circles[i])
+            game.checkCirclePosition(circles[i])
         }
-    
+        }
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
@@ -62,7 +75,15 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if(circle.x < 0){
+                circle.x = canvas.width
+            }
+            if(circle.y < 0){
+                circle.y = canvas.height
+            }
+            if(circle.y > canvas.height){
+                circle.y = 0
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
